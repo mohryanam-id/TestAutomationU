dec993aa37275e7a2dd62d63c32675787eefa4e9
Moh Ryan Alfa Maulana <89937028+mohryanam-id@users.noreply.github.com>
2023-12-03 23:42:48 +0700

    Add project files.

---
+++ TestingWithNunit.Tests/MyFirstNunitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestingWithNunit.Tests
+{
+    internal class MyFirstNunitTests
+    {
+        [Test]
+        public Task IAmDoingGreat()
+        {
+            Console.WriteLine("See you in the test output!");
+            Assert.True(true);
+            return Task.CompletedTask;
+        }
+    }
+}
